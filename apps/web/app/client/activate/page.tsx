91c612b4eec6982c81cfb3f319dc4b38ffa4e4a8
Jarrud van der Merwe <jarrudvandermerwe@jarruds-mini.lan>
2026-03-18 22:41:39 +0200

    Force dynamic client activate page

--- apps/web/app/client/activate/page.tsx
+++ apps/web/app/client/activate/page.tsx
@@ -1,5 +1,7 @@
 import ClientActivateForm from "../../components/ClientActivateForm";
 
+export const dynamic = "force-dynamic";
+
 export default function ClientActivatePage({
   searchParams
 }: {
